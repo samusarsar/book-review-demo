4c9eebc1d046b2d0b6382425b58b2036ddd8e619
samu.sar <samuil.sarandev@gmail.com>
2024-02-25 15:24:55 +0200

    Fix: fixed controllers to include reviews_count and avg_rating info on books

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -20,19 +20,29 @@ class Book extends Model
         return $query->where('title', 'LIKE', "%$title%");
     }
 
-    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    public function scopeWithReviewsCount(Builder $query, $from = null, $to = null): Builder
     {
         return $query->withCount([
             'reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
-        ])
+        ]);
+    }
+
+    public function scopeWithAvgRating(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withAvg([
+            'reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withReviewsCount($from, $to)
             ->orderBy('reviews_count', 'desc');
     }
 
     public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
     {
-        return $query->withAvg([
-            'reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
-        ], 'rating')
+        return $query->withAvgRating($from, $to)
             ->orderBy('reviews_avg_rating', 'desc');
     }
 
@@ -74,5 +84,12 @@ class Book extends Model
         return $query->highestRated(now()->subMonths(6), now())
             ->popular(now()->subMonths(6), now())
             ->minReviews(5);
+    }
+
+    protected static function booted()
+    {
+        static::updated(fn (Book $book) => cache()->forget("book:$book->id"));
+
+        static::deleted(fn (Book $book) => cache()->forget("book:$book->id"));
     }
 }
